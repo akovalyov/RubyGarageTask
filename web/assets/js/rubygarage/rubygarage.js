952775f2c1620d9af75bd047ded5ecc3ed93456c
andrew <andrew.kovalyoff@gmail.com>
2013-07-05 01:24:10 +0300

    done saving of existing project

--- web/assets/js/rubygarage/rubygarage.js
+++ web/assets/js/rubygarage/rubygarage.js
@@ -28,12 +28,39 @@ steal(
                         //@todo replace element;
                     }
                 },
+                //binding on project creation
                 '{Project} created': function (list, ev, item) {
+                    //let's add this project to our stack
                     this.options.projects.push(item);
+                    //display the project
                     this._setProjectView(item);
+                    //and say to worried user to be relaxed - everything is OK
                     this.publish('notification.push', {level: 'success', 'message': 'Project was successfully created'});
+                },
+                //replace title with form
+                '.project_title .icon-edit click': function (el, ev) {
+                    //cache project id to prevent parsing of DOM
+                    var project_id = el.parents("[data-project-id]").attr('data-project-id');
+                    //find needed project
+                    var project = $.grep(this.options.projects, function (project) {
+                        if (project.id == project_id) {
+                            return project;
+                        }
+                    })[0];
+                    $('[data-project-id=' + project_id + '] .project_name').html(Twig.render(project_form_twig, {
+                        project: project
+                    }));
+                },
+                '.project_title form submit': function (el, ev) {
+                    ev.preventDefault();
+                    new Project({
+                        name: el.find('[name=name]').val(),
+                        id: el.parents("[data-project-id]").attr('data-project-id')
+                    }).save(function(){
+                            //@todo edit callback
+                            console.log('saved');
+                        });
                 }
-
             })
             ;
             $.Controller('TaskController', {}, {
@@ -44,7 +71,9 @@ steal(
                     ev.preventDefault();
                     var project = new Project({name: el.find('[name = name]').val()});
                     project.save(function () {
+                        //push new project to controller to allow it to add the project to the DOM
                         $('[data-controller=project]').project(project);
+                        //close modal window
                         $('[data-dismiss=modal]').trigger('click');
 
                     });
@@ -60,25 +89,36 @@ steal(
                 }
             });
             $.Model('Project', {
+                fakeData: [
+                    {id: 1, name: 'Project 1'},
+                    {id: 2, name: 'Project 2'},
+                    {id: 3, name: 'Project 3'}
+                ],
                 findAll: function (params, success, error) {
                     return $.ajax({
                         url: "/projects.json",
                         data: params,
                         dataType: "json",
                         fixture: function () {
-                            return [
-                                [
-                                    {id: 1, name: 'Project 1'},
-                                    {id: 2, name: 'Project 2'},
-                                    {id: 3, name: 'Project 3'}
-                                ]
-                            ];
+                            return [Project.fakeData];
 
                         },
                         success: success
                     })
                 },
-                findOne: 'GET /projects/{id}.json',
+                findOne: function (id, success, error) {
+                    return $.ajax({
+                        url: "/projects/" + id + ".json",
+                        dataType: "json",
+                        fixture: function () {
+                            return [$.grep(Project.fakeData, function (project) {
+                                return project.id == id;
+                            })[0]];
+
+                        },
+                        success: success
+                    })
+                },
                 create: 'POST /projects.json',
                 update: 'PUT /projects/{id}.json',
                 destroy: 'DELETE /projects/{id}.json'
@@ -95,7 +135,8 @@ steal(
                 $('[data-controller=windows]').windows('initialize', ev
                 );
             });
-        });
+        })
+        ;
     })
 
 ;
